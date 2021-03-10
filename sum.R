cf3828884e3bfdcf350d6be50dce1f0923bede35
email <vivek_1rvd@rediffmail.com>
2021-03-10 23:53:01 +0530

    commit data

---
+++ sum.R
@@ -0,0 +1,35 @@
+new_sum <- function (v1,v2) {
+  result <- v1 * v2
+  result
+}
+download.file(url,destfile = "./Cam.xlsx")
+
+> read_xlsx("./cam.xlsx",sheetIndex =1, Header = TRUE)
+>  read_xlsx("./cam.xlsx")
+
+> install.packages("XLConnect")
+
+package 'XLConnect' successfully unpacked and MD5 sums checked
+
+
+library(XLConnect)
+
+library(XML)
+url <- "https://www.w3schools.com/xml/simple.xml"
+doc <- xmlTreeParse(url,useInternal = TRUE)
+
+doc <- xmlTreeParse(url)
+
+doc <- xmlTreeParse(url,useInternalNodes = TRUE)
+
+doc <- xmlTreeParse(url, ignoreBlanks = TRUE)
+library(xml2)
+doc <- xmlTreeParse(url,useInternal = TRUE)
+
+url <- "http://www.w3schools.com/xml/simple.x
+
+#handle google
+
+google = handle("http://google.com")
+w1 = GET(handle = google, Path = "/")
+w2 = GET(handle = google, Path = "search")
